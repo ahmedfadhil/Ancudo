ffd51abb1c3d6a8c2d7e75846ce8f2acc3d4d89f
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-09-11 13:19:23 +0200

    added validation and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,28 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
   def new
     @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      flash[:success] = "Success!, Message was saved successfully."
+      redirect_to new_contact_path
+
+    else
+      flash[:danger] = "Error!, Unexpected Error Occured."
+      redirect_to new_contact_path
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comment)
   end
 end
