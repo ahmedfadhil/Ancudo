3c4c3d245aeb5cff2896dd8368b7caaf48020c41
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2016-09-26 22:47:56 +0200

    added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,24 @@
 class ProfilesController < ApplicationController
-def new
-  @user = User.find(params[:user_id])
-  @profile = @user.build_profile
-end
+  def new
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @profile = @user.build_profile(profile_params)
+
+    if @profile.save
+      flash[:success]="The profile was created successfully!"
+      redirect_to user_path(params[:user_id])
+    else
+      flash[:danger]="An error has occurred."
+      render action: :new
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
 end
